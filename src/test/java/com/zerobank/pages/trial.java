d86a4387fbed48d28c1e3ffd8814756d5a075f5d
NuriYildiz <26silentdancer26@gmail.com>
2021-02-25 13:30:51 +0200

    second commit nuri

--- src/test/java/com/zerobank/pages/trial.java
+++ src/test/java/com/zerobank/pages/trial.java
@@ -3,6 +3,7 @@ package com.zerobank.pages;
 public class trial {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("branch Nuri");
     }
 
 }
